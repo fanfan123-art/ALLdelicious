ff7eed1c1db3fb2dc51de7d22262134ca77c994f
kite-yuan <494116852@qq.com>
2019-12-29 11:11:23 +0800

    29

--- src/homepage/HomePage.js
+++ src/homepage/HomePage.js
@@ -19,7 +19,7 @@ export default class HomePage extends React.Component {
                            百馐百味百盘馔，一菜一格一品花。</p>
                     </div>
                     <div className={HomePageCss.logo01}>
-                    <Router pash="/Chuan"><h1>川</h1></Router>
+                    <Link to="/Chuan"><h1>川</h1></Link>
                     </div>
                 </div>
                 <div className={HomePageCss.lu}>
--- src/index.js
+++ src/index.js
@@ -7,8 +7,14 @@ import Register from './register/Register';
 import {HashRouter as Router , Route , Switch} from 'react-router-dom';
 import { createBrowserHistory } from "history";
 import HomePage from './homepage/HomePage';
-import Chuan from './homepage/chuan/Chuan'
+import Chuan from './homepage/chuan/Chuan';
 import Zhe from './homepage/zhe/Zhe';
+import Lu from './homepage/lu/Lu';
+import Hui from './homepage/hui/Hui';
+import Min from './homepage/min/Min';
+import Su from './homepage/su/Su';
+import Xiang from './homepage/xiang/Xiang';
+import Yue from './homepage/yue/Yue';
 
 export default class Client extends React.Component{   
     render(){
@@ -23,6 +29,12 @@ export default class Client extends React.Component{
                     <Route path="/register" component={Register}></Route>
                     <Route path="/chuan" component={Chuan}></Route>
                     <Route path="/zhe" component={Zhe}></Route>
+                    <Route path="/lu" component={Lu}></Route>
+                    <Route path="/hui" component={Hui}></Route>
+                    <Route path="/min" component={Min}></Route>
+                    <Route path="/su" component={Su}></Route>
+                    <Route path="/xiang" component={Xiang}></Route>
+                    <Route path="/yue" component={Yue}></Route>
                 </Switch>
                 </Router>
             </div>
